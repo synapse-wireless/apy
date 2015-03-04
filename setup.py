13fcb603cf817dedb6d523005b69dbdcc9b0b73d
Mark Guagenti <mark.guagenti@synapse-wireless.com>
2015-03-04 12:29:42 -0600

    Don't need version in setup.py with vcversioner

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 
-
-import __init__
-
-
 setup(name='apy',
       author='David Ewing, Mark Guagenti', 
       url='http://github.com/mgenti/apy', 
       maintainer='Mark Guagenti', 
-      version=str(__init__.__version__), 
       package_dir={'apy': ''},
       setup_requires=['vcversioner'],
       vcversioner={
