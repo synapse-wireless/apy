bac0a53657c891c6ff1ef79616681fd967a8a4c5
Mark Guagenti <mark.guagenti@synapse-wireless.com>
2015-03-04 12:25:31 -0600

    Updating for using Git

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 #The __init__.py files are required to make Python treat the directories as containing packages
-__version__ = 2.40
+from _version import __version__, __revision__
--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ import __init__
 
 setup(name='apy',
       author='David Ewing, Mark Guagenti', 
-      url='http://sourceforge.net/projects/apy', 
+      url='http://github.com/mgenti/apy', 
       maintainer='Mark Guagenti', 
       version=str(__init__.__version__), 
-      package_dir={'apy': ''}, 
+      package_dir={'apy': ''},
+      setup_requires=['vcversioner'],
+      vcversioner={
+          'version_module_paths': ['_version.py'],
+      }, 
       packages=['apy'], 
       )
